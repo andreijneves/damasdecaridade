d2d480ebc086d0dda07017c7583221b19ebb11b8
andreijneves <andreijneves@yahoo.com.br>
2017-08-01 17:24:11 -0300

    ajsutes

--- wp-content/themes/damasdecaridade/404.php
+++ wp-content/themes/damasdecaridade/404.php
@@ -1,24 +1,11 @@
 <?php get_header(); ?>
 <section>
-	<div class="area-blog-titulo"><img src="<?php bloginfo('template_directory'); ?>/img/bullet-internas.png"\>TITULO DO POST</div>
+	<div class="area-blog-titulo"><img src="<?php bloginfo('template_directory'); ?>/img/bullet-internas.png"\>OPS! Temos um problema!</div>
 
 	<div class="texto-internas">
-		<p>
-		Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"Não há ninguém que ame a dor por si só, que a busque e queira tê-la, simplesmente por ser dor..."
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae aliquam diam. Sed cursus egestas laoreet. Quisque egestas sagittis posuere. Cras id nisi a enim placerat placerat et vel quam. In pretium semper urna et egestas. Maecenas rutrum neque et erat iaculis, vel tristique risus tristique. Pellentesque dapibus condimentum lectus ac laoreet. Morbi rhoncus in metus quis mollis.
-
-Suspendisse aliquet facilisis neque, at tincidunt nibh lobortis vitae. Sed sagittis vestibulum pulvinar. Etiam eleifend nunc quam, sit amet hendrerit odio commodo sed. Sed consequat eros scelerisque est dictum, eu tincidunt ante sodales. Suspendisse lacinia nunc bibendum tincidunt ullamcorper. Donec eu malesuada tortor. Mauris at nisi arcu. Pellentesque ultricies sed dui quis pretium.
-
-Pellentesque eget iaculis velit. Phasellus nisi purus, lobortis ut felis vel, auctor eleifend tortor. Sed ac consectetur odio. Sed viverra malesuada magna ut dignissim. Donec in nibh id risus semper commodo non tempor quam. Nulla mauris libero, faucibus tincidunt suscipit nec, finibus porttitor lectus. Integer sed diam eget nisl aliquet tincidunt. Sed fringilla elit et nisl mollis interdum id at ex. Vestibulum interdum velit sit amet diam mollis, in bibendum elit commodo.
-
-Phasellus commodo sem in metus aliquet, euismod mollis dolor congue. Etiam ultrices iaculis justo, a vulputate enim commodo quis. Nam id varius neque, sed posuere purus. Proin odio quam, tincidunt at commodo vitae, tempus a erat. Vivamus volutpat lacus sed leo consequat mattis. Sed ipsum ipsum, porta sit amet ligula rhoncus, aliquet vestibulum tellus. Mauris scelerisque in sapien in imperdiet.
-
-Mauris commodo elit tincidunt sapien bibendum, sodales vehicula ligula commodo. Etiam in iaculis mi, vitae maximus ex. Quisque in scelerisque ipsum. Curabitur a feugiat felis. Sed at hendrerit dui. Integer et eros ac sem feugiat imperdiet quis dapibus dolor. Cras elementum magna felis, vitae condimentum nisl tempor sed. Aenean consequat volutpat finibus. Nulla id semper nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec varius ipsum at consectetur vulputate. Ut id facilisis turpis, eu condimentum magna. Proin blandit eu mauris a tristique.
-
-Foram gerados 5 parágrafos, 331 palavras e 2245 bytes de Lorem Ipsum
-		</p>
+		<p>O Conteúdo que voce está tentando acessar não encontra-se mais disponível.<br><br>Voce poderá continuar a navegação normalmente ou pode nos enviar um contato informando a indisponibilidade deste arquivo.
+		<br>
+		Nós agradecemos sua atenção e pedimos desculpas pelo transtorno!	</p>
 	</div>
 	</section>	
 <?php get_footer(); ?>
---
+++ wp-content/themes/damasdecaridade/front-page.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>
+<section>
+		<div class="destaques">
+			<div style="background-image: url(<?php bloginfo('template_directory'); ?>/img/caridade.jpg);">
+				<div class="dtk-label">
+					<span class="dtk-tit">Como exercer a caridade com a Damas de Caridade</span><br>
+					estamos disponíveis para doações que nos ajudarão no trabalho socio-educativo da associação...
+				</div>
+			</div>
+			<div style="background-image: url(<?php bloginfo('template_directory'); ?>/img/pessoinhas.jpeg);">
+				<div class="dtk-label">
+					<span class="dtk-tit">Conheça o trabalho socio-educativo da ADAC</span><br>
+					estamos disponíveis para doações que nos ajudarão no trabalho socio-educativo da associação...
+				</div>
+			</div>
+			<div style="background-image: url(<?php bloginfo('template_directory'); ?>/img/frades.jpg);">
+				<div class="dtk-label">
+					<span class="dtk-tit">Nosso Patrono</span><br>
+					Conheça São Vicente de Paulo...
+				</div>
+			</div>
+
+		</div>
+		<div class="area-blog-titulo">
+			<img src="<?php bloginfo('template_directory'); ?>/img/blog.png" \>De Olho no Blog - Sempre atentos as novidades...
+		</div>
+
+		<div class="area-news">
+			<div class="news-container">
+				<div class="item-news">
+					<span class="tit">
+						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \>Metodologia de Ensino socio-Educativo
+					</span>		<br \><br \>
+					Nos estudos da Associação estamos periodicamente buscando atualização no compo socio-educativo e pedagogico no trabalho com crianças em situação de ...		
+					<div class="leiamais"><a href="#">Leia Mais...</a></div>
+				</div>
+				<div class="item-news">
+					<span class="tit">
+						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \>Ação Social em Julho
+					</span>	<br \><br \>
+					Leia mais sobre o trabalho social prestado no mês de julho e fique por dentro das ações da associação...
+					<div class="leiamais"><a href="#">Leia Mais...</a></div>
+				</div>
+				<div class="item-news">
+					<span class="tit">
+						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \>Precisamos da sua ajuda!
+					</span>	<br \><br \>
+					seja um associado da Adac e nos ajude através da doação de equipamentos, alimento não perecível, roupas, brinquedos e doações financeiras pelo pagseguro, depósito em conta ou ainda trazendo em mãos e vindo conhecer a associação presencialmente.
+					<div class="leiamais"><a href="#">Leia Mais...</a></div>
+				</div>
+			</div>			
+		</div>
+		<div class="doe-aqui">
+				<img src="<?php bloginfo('template_directory'); ?>/img/bannerdoacoes.png" \>
+		</div>
+		<div class="area-galeria-titulo">
+			<img src="<?php bloginfo('template_directory'); ?>/img/icon-foto.jpg"\>
+			Conheça mais a Associação das Damas de Caridade de Florianópolis
+		</div>
+
+		<div class="area-gal">			
+				<div class="item-gal" style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal1.jpg);">
+						<div class="gal-label">
+							<span class="gal-tit">Galeria Teste 1</span>
+						</div>				
+				</div>
+				<div class="item-gal" style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal2.jpg);">
+					<div class="gal-label">
+							<span class="gal-tit">Galeria Teste 2</span>
+						</div>
+				</div>
+				<div class="item-gal"  style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal3.jpg);"> 
+					<div class="gal-label">
+							<span class="gal-tit">Galeria Teste 3</span>
+						</div>
+				</div>	
+				<div class="item-gal"  style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal3.jpg);"> 
+					<div class="gal-label">
+							<span class="gal-tit">Galeria Teste 3</span>
+						</div>
+				</div>				
+		</div>
+
+</section>
+<?php get_footer(); ?>
+
+	
