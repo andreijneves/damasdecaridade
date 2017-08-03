e81454b3019dbdf7614b11fd28b6765ac6a837e1
andreijneves <andreijneves@yahoo.com.br>
2017-08-03 19:04:04 -0300

    index ajustado

--- wp-content/themes/damasdecaridade/index.php
+++ wp-content/themes/damasdecaridade/index.php
@@ -4,24 +4,18 @@ get_header();
 ?>
 <section>
 		<div class="destaques">
-			<div style="background-image: url(<?php bloginfo('template_directory'); ?>/img/caridade.jpg);">
-				<div class="dtk-label">
-					<span class="dtk-tit">Como exercer a caridade com a Damas de Caridade</span><br>
-					estamos disponíveis para doações que nos ajudarão no trabalho socio-educativo da associação...
+		<?php
+				global $post;
+				$args = array('cat' => '3' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) : setup_postdata($post); ?>
+				<div style="background-image: url(<?php the_post_thumbnail_url() ?>">
+					<div class="dtk-label">						
+						<span class="dtk-tit"><?php the_title() ?></span>
+						<?php the_excerpt() ?> 
+					</div>
 				</div>
-			</div>
-			<div style="background-image: url(<?php bloginfo('template_directory'); ?>/img/pessoinhas.jpeg);">
-				<div class="dtk-label">
-					<span class="dtk-tit">Conheça o trabalho socio-educativo da ADAC</span><br>
-					estamos disponíveis para doações que nos ajudarão no trabalho socio-educativo da associação...
-				</div>
-			</div>
-			<div style="background-image: url(<?php bloginfo('template_directory'); ?>/img/frades.jpg);">
-				<div class="dtk-label">
-					<span class="dtk-tit">Nosso Patrono</span><br>
-					Conheça São Vicente de Paulo...
-				</div>
-			</div>
+				<?php endforeach; ?>
 
 		</div>
 		<div class="area-blog-titulo">
@@ -30,28 +24,24 @@ get_header();
 
 		<div class="area-news">
 			<div class="news-container">
+				<?php
+				global $post;
+				$args = array('cat' => '-4' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) : setup_postdata($post); ?>
 				<div class="item-news">
 					<span class="tit">
-						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \>Metodologia de Ensino socio-Educativo
+						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \><a href="<?php the_permalink() ?>"><?php the_title() ?>
 					</span>		<br \><br \>
-					Nos estudos da Associação estamos periodicamente buscando atualização no compo socio-educativo e pedagogico no trabalho com crianças em situação de ...		
-					<div class="leiamais"><a href="#">Leia Mais...</a></div>
+					<?php strip_tags(the_excerpt()) ?>	</a>				
 				</div>
-				<div class="item-news">
-					<span class="tit">
-						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \>Ação Social em Julho
-					</span>	<br \><br \>
-					Leia mais sobre o trabalho social prestado no mês de julho e fique por dentro das ações da associação...
-					<div class="leiamais"><a href="#">Leia Mais...</a></div>
-				</div>
-				<div class="item-news">
-					<span class="tit">
-						<img src="<?php bloginfo('template_directory'); ?>/img/snipet.png" \>Precisamos da sua ajuda!
-					</span>	<br \><br \>
-					seja um associado da Adac e nos ajude através da doação de equipamentos, alimento não perecível, roupas, brinquedos e doações financeiras pelo pagseguro, depósito em conta ou ainda trazendo em mãos e vindo conhecer a associação presencialmente.
-					<div class="leiamais"><a href="#">Leia Mais...</a></div>
-				</div>
-			</div>			
+				<?php endforeach; ?>
+
+			</div>
+
+				
+				
+						
 		</div>
 		<div class="doe-aqui">
 				<img src="<?php bloginfo('template_directory'); ?>/img/bannerdoacoes.png" \>
@@ -62,26 +52,19 @@ get_header();
 		</div>
 
 		<div class="area-gal">			
-				<div class="item-gal" style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal1.jpg);">
-						<div class="gal-label">
-							<span class="gal-tit">Galeria Teste 1</span>
-						</div>				
-				</div>
-				<div class="item-gal" style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal2.jpg);">
-					<div class="gal-label">
-							<span class="gal-tit">Galeria Teste 2</span>
-						</div>
-				</div>
-				<div class="item-gal"  style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal3.jpg);"> 
-					<div class="gal-label">
-							<span class="gal-tit">Galeria Teste 3</span>
-						</div>
-				</div>	
-				<div class="item-gal"  style="background-image: url(<?php bloginfo('template_directory'); ?>/img/gal3.jpg);"> 
-					<div class="gal-label">
-							<span class="gal-tit">Galeria Teste 3</span>
-						</div>
-				</div>				
+				<?php
+				global $post;
+				$args = array('posts_per_page' => 4, 'cat' => '4' );
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) : setup_postdata($post); ?>
+						
+					<div class="item-gal" style="background-image: url(<?php the_post_thumbnail_url() ?>); cursor:pointer;" onclick="location.href='<?php the_permalink() ?>';">
+							<div class="gal-label">
+								<span class="gal-tit"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></span>
+							</div>				
+					</div>		
+					
+				<?php endforeach; ?>				
 		</div>
 
 </section>
